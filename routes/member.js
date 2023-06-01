0dbc9d2bc59d4f443866444fd1269faf2bda12fc
cody <109947297+anydevil0812@users.noreply.github.com>
2023-06-01 20:02:38 +0900

    member API reponse에 msg 추가

--- routes/member.js
+++ routes/member.js
@@ -13,9 +13,9 @@ module.exports = function () {
         res.send("sql error");
       } else {
         if (result.length != 0) {
-          res.send("new"); // 기존회원인 경우
+          res.json({ msg: "기존회원입니다.", type: "exist" }); // 기존회원인 경우
         } else {
-          res.send("exist"); // 신규회원인 경우
+          res.json({ msg: "신규회원입니다.", type: "new" }); // 신규회원인 경우
         }
       }
     });
@@ -31,9 +31,10 @@ module.exports = function () {
       } else {
         if (result.length != 0) {
           req.session.member_info = result[0]; // 로그인을 하면 세션에 모든 유저 정보를 저장
-          res.json({ result: true }); // 로그인 성공
+          console.log(req.session.member_info);
+          res.json({ msg: "로그인에 성공하였습니다.", result: true }); // 로그인 성공
         } else {
-          res.json({ result: false }); // 로그인 실패
+          res.json({ msg: "로그인에 실패하였습니다.", result: false }); // 로그인 실패
         }
       }
     });
@@ -41,7 +42,7 @@ module.exports = function () {
 
   // 로그아웃
   router.get("/logout", (req, res) => {
-    req.session.destroy(); // 세션 삭제
+    req.session.destroy(); // 세션 제거
     res.json({ state: 0 }); // 로그아웃 상태 (세션 소멸)
   });
 
@@ -52,9 +53,9 @@ module.exports = function () {
     console.log(_wallet, _nickname);
     con.query(sql.signup, [_wallet, _nickname], (err, result) => {
       if (err) {
-        res.json({ result: false }); // 회원가입 실패
+        res.json({ msg: "회원가입에 실패하였습니다.", result: false }); // 회원가입 실패
       } else {
-        res.json({ result: true }); // 회원가입 성공
+        res.json({ msg: "회원가입이 완료되었습니다.", result: true }); // 회원가입 성공
       }
     });
   });
@@ -65,9 +66,9 @@ module.exports = function () {
     console.log(_wallet);
     con.query(sql.withdrawl, [_wallet], (err, result) => {
       if (err) {
-        res.json({ result: false }); // 회원탈퇴 실패
+        res.json({ msg: "회원탈퇴에 실패하였습니다.", result: false }); // 회원탈퇴 실패
       } else {
-        res.json({ result: true }); // 회원탈퇴 성공
+        res.json({ msg: "회원탈퇴가 완료되었습니다.", result: true }); // 회원탈퇴 성공
       }
     });
   });
